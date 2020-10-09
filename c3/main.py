5405974084d0276e2509f3011f2518c36983911c
Ryan Wickman <rwickman@memphis.edu>
2020-10-09 15:11:34 -0500

    Add plotting hyperparamaters H tuning and feature scaling

--- c3/main.py
+++ c3/main.py
@@ -1,6 +1,8 @@
 import numpy as np
-import os
+import math, os 
 from trainer import TrainerBN
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 def load_bi_data():
     data_dir = "datasets/bi-class"
@@ -12,7 +14,39 @@ def load_bi_data():
             np.load(os.path.join(data_dir, data_file)))
     return bi_data_dict
 
-bi_data_dict = load_bi_data()
+def scale_dataset(dataset):
+    scaler = StandardScaler().fit(dataset["train_X"])
+    dataset["train_X"] = scaler.transform(dataset["train_X"])
+    dataset["test_X"] = scaler.transform(dataset["train_X"])
 
-trainer_bn = TrainerBN(bi_data_dict["iris"], patience=15)
-trainer_bn.k_fold_cv(5, 1)
+def _plot_H_loss(val_losses_dict, H_list):
+    fig, axs = plt.subplots(math.ceil(len(val_losses_dict)/2), 2)
+    for i, name in enumerate(val_losses_dict):
+        axs[i//2][i%2].set_title("BCE Validation Loss vs H for {} Dataset".format(name))
+        axs[i//2][i%2].set_xlabel("H")
+        axs[i//2][i%2].set_ylabel("BCE Validation Loss")
+        axs[i//2][i%2].set_xticks(H_list)
+        axs[i//2][i%2].grid(True)
+        axs[i//2][i%2].plot(H_list, val_losses_dict[name])
+    fig.suptitle("Binary Classification Neural Network H Hyperparameter Tuning", fontsize=24)
+    fig.subplots_adjust(hspace=0.40)
+    if math.ceil(len(val_losses_dict)/2) * 2 > len(val_losses_dict):
+        fig.delaxes(axs[math.ceil(len(val_losses_dict)/2)-1][1])
+    plt.show()
+
+def main():
+    # Perform hyperparameter tuning on number for
+    # the number units in the hidden layer for
+    # binary classification tasks
+    H_list = np.arange(1,11)
+    val_losses_dict = {}
+    bi_data_dict = load_bi_data()
+    for name, dataset in bi_data_dict.items():
+        scale_dataset(dataset)
+        trainer_bn = TrainerBN(dataset, H_list, patience=30, epochs=1)
+        val_losses_dict[name] = trainer_bn.k_fold_cv(5, 1)
+
+    _plot_H_loss(val_losses_dict, H_list)
+
+if __name__ == "__main__":
+    main()
--- c3/trainer.py
+++ c3/trainer.py
@@ -6,40 +6,43 @@ import numpy as np
 from sklearn.model_selection import KFold
 from models import BinaryNetwork
 
+
 class TrainerBN:
-    def __init__(self, 
+    def __init__(self,
                 dataset,
-                lr=1e-3,
+                H_list,
+                lr=1e-2,
                 epochs=25,
                 batch_size=8,
                 early_stopping=True,
                 patience=10,
-                num_rand_init=1):
+                num_rand_init=1,
+                decay_rate=0.99):
         """Create TrainerBN to train and perform hyperparameter tuning.
 
         Args:
             dataset: the dictionary of numpy training and testing data
+            H_list: list of H values to test for on cross-validation
             lr: learning rate
             epochs: the maximum number of times to train over the dataset
             batch_size: the size of mini-batches
             early_stopping: should stop after validation loss does not decrease past min after patience steps
             patience: the number of steps to wait before stopping for early_stopping
-        """
+            decay_rate: the decay rate to use for exponential learning rate decay
         
+        """
         self._lr = lr
         self._epochs = epochs
         self._batch_size = batch_size
-        # 
         self._early_stopping = early_stopping
         self._patience = patience
-
-        
         self._num_rand_init = num_rand_init
-
+        self._decay_rate = decay_rate
+        
         # Combines Sigmoid with binary cross entropy loss
         self._loss_fn = nn.BCEWithLogitsLoss()
         
-        self._H_list = np.arange(2, 10)#np.arange(2, 11)
+        self._H_list = H_list
         self._train_X = dataset["train_X"]
         self._train_Y = dataset["train_Y"][:, np.newaxis]
         self._test_X = dataset["test_X"]
@@ -53,7 +56,10 @@ class TrainerBN:
             n_splits: the amount of partition of the training dataset
             num_init: the amount of times to test one hyperparameter.
         """
-        kf = KFold(n_splits=n_splits)
+        kf = KFold(n_splits=n_splits, shuffle=True)
+
+        best_bi_net = None
+        min_val_loss = None
 
         # Iterate over all the possible number of units
         avg_val_losses = []
@@ -67,9 +73,10 @@ class TrainerBN:
                     # Build the network
                     bi_net = BinaryNetwork(self._train_X.shape[1], H)    
                     optimizer = optim.Adam(bi_net.parameters(), lr=self._lr)
-                    
+                    # The LR Decay Scheduler
+                    lr_scheduler = optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=self._decay_rate)
                     # Train on current training partition
-                    self._train_fold(bi_net, optimizer, train_idx, test_idx)
+                    self._train_on_fold(bi_net, optimizer, train_idx, test_idx, lr_scheduler)
                     
                     # Compute fold loss
                     outputs = bi_net(torch.from_numpy(self._train_X[test_idx]).float())
@@ -86,8 +93,9 @@ class TrainerBN:
             avg_val_losses.append(min_avg_loss)
 
         print(avg_val_losses)
+        return avg_val_losses
 
-    def _train_fold(self, bi_net, optimizer, train_idx, test_idx):
+    def _train_on_fold(self, bi_net, optimizer, train_idx, test_idx, lr_scheduler):
         train_fold_X = self._train_X[train_idx]
         train_fold_Y = self._train_Y[train_idx]
         test_fold_X = self._train_X[test_idx]
@@ -98,7 +106,8 @@ class TrainerBN:
             min_val_loss = None
             steps_past_min = 0
         for e_i in range(self._epochs):
-            print("Training on epoch ", e_i)
+            if e_i % 5 == 0:
+                print("Training on epoch ", e_i)
             # Train on mini-batches
             total_batch_loss = 0
             trainloader = torch.utils.data.DataLoader(list(zip(train_fold_X, train_fold_Y)), batch_size = self._batch_size)
@@ -119,9 +128,10 @@ class TrainerBN:
                 # Test on validation set
                 outputs = bi_net(torch.from_numpy(test_fold_X).float())
                 # Compute loss on batch
-                val_batch_loss = self._loss_fn(outputs, torch.from_numpy(test_fold_Y).float()) 
-                
+                val_batch_loss = self._loss_fn(outputs, torch.from_numpy(test_fold_Y).float()).item()
                 optimizer.zero_grad()
+                
+                #print("lr_scheduler.get_lr(): ", lr_scheduler.get_lr())
                 if self._early_stopping:
                     if min_val_loss == None or min_val_loss >= val_batch_loss:
                         min_val_loss = val_batch_loss
@@ -133,5 +143,8 @@ class TrainerBN:
                         print("EARLY STOPPING ON EPOCH ", e_i)
                         return
 
+            lr_scheduler.step()
+
 
             print("Batch Loss: ", total_batch_loss)
+
