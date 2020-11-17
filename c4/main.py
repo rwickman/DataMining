8df4cfb8fd908ab7bcd82ca15b48e1b64d01be3d
Ryan Wickman <rwickman@memphis.edu>
2020-11-17 12:08:43 -0600

    Add CLI arguments

--- c4/main.py
+++ c4/main.py
@@ -4,10 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 from scipy.sparse import csr_matrix
 import numpy as np
-
-
-ALPHA = 0.15
-PR_ITER = 20
+import argparse
 
 stemmer = PorterStemmer()
 def clean_text(text):
@@ -20,8 +17,8 @@ def clean_text(text):
     text = newline_regex.sub(" ", text)
 
     # Remove all non alphabetic characters
-    alphanumeric_regex = re.compile("[^ a-z0-9]")
-    text = alphanumeric_regex.sub("", text)
+    alphabetic_regex = re.compile("[^ a-z]")
+    text = alphabetic_regex.sub("", text)
 
     # Perform stemming on every word
     stemmed_words = []
@@ -31,13 +28,13 @@ def clean_text(text):
     return stemmed_words
 
 
-def top_pr(proximity_matrix, cosine_threshold):
+def top_pr(args, proximity_matrix, cosine_threshold):
     num_nodes = proximity_matrix.shape[0]
 
     # Create sparse-encoding of the adjacency matrix
     adj_matrix = csr_matrix(proximity_matrix.shape)
     pr_scores = np.repeat(1, num_nodes)
-    max_sim = 0
+    num_edges = 0
     for i in range(num_nodes):
         for j, sim in enumerate(proximity_matrix[i]):
             # Don't create self-cycles
@@ -45,7 +42,7 @@ def top_pr(proximity_matrix, cosine_threshold):
                 continue
             if sim >= cosine_threshold:
                 adj_matrix[i,j] = 1
-                max_sim = max(sim, max_sim)
+                num_edges += 1
 
     # Get out degrees of each node
     out_degrees = adj_matrix.sum(axis=-1)
@@ -54,40 +51,68 @@ def top_pr(proximity_matrix, cosine_threshold):
     out_degrees[out_degrees == 0] = 1
 
     # Create transition probability
-    M = adj_matrix.multiply(1/out_degrees)
-
-    for i in range(PR_ITER):
-        pr_scores = ALPHA * (1/num_nodes) + (1-ALPHA) * M.T.dot(pr_scores.T) 
+    M = adj_matrix.multiply(1/out_degrees).T
+    #print(M)
+    for i in range(args.pr_iter):
+        print(i)
+        pr_scores = args.alpha * (1/num_nodes) + (1-args.alpha) * M.dot(pr_scores)
         #print(pr_scores)
 
     # Get the papers with higher PR score
-    rank_by_pr = np.argsort(pr_scores)
-    top_10 =  rank_by_pr[:10]
-    return top_10
+    #rank_by_pr = np.argsort(pr_scores)
+    
+    
+   
+    return pr_scores, num_edges
 
 
+def create_results(args):
+    # Read in the papers
+    papers = []
+    with open(args.input) as f:
+        for line in f:
+            papers.append(json.loads(line))
+
+    corpus = []
+    paper_titles = []
+    #for i in range(len(papers)):
+    for i in range(len(papers)):
+        corpus.append(clean_text(papers[i]["abstract"]))
+        paper_titles.append(papers[i]["title"])
+
+    # Fit TF-IDF on corpus
+    vectorizer = TfidfVectorizer(tokenizer=lambda x: x, lowercase=False)
+    X = vectorizer.fit_transform(corpus)
+
+    # Pairwise cosine similarity on abstracts
+    proximity_matrix = cosine_similarity(X, X)
+
+    # Compute PageRank and output results
+    with open(args.output, "w") as f:
+        for threshold in args.thetas:
+            print(threshold)
+            pr_scores, num_edges = top_pr(args, proximity_matrix, threshold)
+            ranked_papers = sorted(zip(pr_scores, paper_titles), key=lambda x: (-x[0], x[1]) )
+            f.write("θ = {}, |V| = {}, |E| = {},\n".format(threshold, proximity_matrix.shape[0], num_edges))
+            for i, top_pair in enumerate(ranked_papers[:args.num_top]):
+                f.write(top_pair[1].replace("\n", "") + ";\n")
 
 
-covid_papers = "CovidPaper.json"
+def main(args):
+    create_results(args)
 
-papers = []
-with open(covid_papers) as f:
-    for line in f:
-        papers.append(json.loads(line))
-
-# papers_dict = json.load(f)
-# print(type(papers_dict))
-corpus = []
-for i in range(len(papers)):
-    corpus.append(clean_text(papers[i]["abstract"]))
-
-# Fit TF-IDF on corpus
-vectorizer = TfidfVectorizer(tokenizer=lambda x: x, lowercase=False)
-X = vectorizer.fit_transform(corpus)
-
-# Pairwise cosine similarity on abstracts
-proximity_matrix = cosine_similarity(X, X)
-
-for threshold in [0.25, 0.5, 0.75]:
-    top_10 = top_pr(proximity_matrix, threshold)
-    print(top_10)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--alpha", type=float, default=0.15,
+            help="Alpha value for PageRank.")
+    parser.add_argument("--pr_iter", type=int, default=20,
+            help="Number of PageRank iterations.")
+    parser.add_argument("--num_top", type=int, default=10,
+            help="Number of top PageRank title results to write.")
+    parser.add_argument("--thetas", type=float, nargs="*", default=[0.25,0.5,0.75],
+            help="Cosine threshold to create an edge for graph.")
+    parser.add_argument("--output", default="results.txt",
+            help="Output path/filename.")
+    parser.add_argument("--input", default="CovidPaper.json",
+            help="Input JSON path/filename.")
+    main(parser.parse_args())
